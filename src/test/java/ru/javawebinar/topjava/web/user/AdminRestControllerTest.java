b61594a206e537b1102f71cab490361e5fd9de4b
Sndolgov <Snd251084>
2018-10-26 19:39:33 +0300

    7_08_JUnit5

--- src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/AdminRestControllerTest.java
@@ -1,21 +1,20 @@
 package ru.javawebinar.topjava.web.user;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.http.MediaType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 
-class AdminRestControllerTest extends AbstractControllerTest {
+public class AdminRestControllerTest extends AbstractControllerTest {
 
     private static final String REST_URL = AdminRestController.REST_URL + '/';
 
     @Test
-    void testGet() throws Exception {
+    public void testGet() throws Exception {
         mockMvc.perform(get(REST_URL + ADMIN_ID))
                 .andExpect(status().isOk())
                 .andDo(print())
